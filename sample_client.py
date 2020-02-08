526b1046db4fa2192bbfae4770c12407bcc0c63c
uri <utayvah@gmail.com>
2020-02-07 20:28:25 -0500

    add header for sample client

--- sample_client.py
+++ sample_client.py
@@ -1,3 +1,11 @@
+# takes a file name from the command line and converts it, generating a csv
+# in the same location as the argument
+#
+# Example: 
+# python3 sample_client.py filename.spc
+#
+# would generate a csv named filename-DataSet[num].csv for every dataset in the spc file
+
 import getSpectrum
 import sys
 
